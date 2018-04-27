d020f449abe71e24e3a4ee5909abf843b815eb46
Giildo <giildo.jm@gmail.com>
2018-04-27 09:24:10 +0200

    Création de la page d'ajout des tricks

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -4,12 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Entity\Trick;
+use App\Entity\User;
+use App\Form\TrickType;
+use DateTime;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 /**
  * @Route("/trick", name="tricks_")
@@ -19,8 +25,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class TricksController extends AppController
 {
     /**
-     * @Route("/show/{category}/{slug}", name="show", requirements={"category"="\w+", "slug"="\w+"})
-     * @param RegistryInterface $doctrine
+     * @var RegistryInterface
+     */
+    private $doctrine;
+
+    public function __construct(Environment $twig, RegistryInterface $doctrine)
+    {
+        parent::__construct($twig);
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @Route("/{category}/{slug}", name="show", requirements={"category"="\w+", "slug"="\w+"})
      * @param FormFactoryInterface $formBuilder
      * @param string $slug
      * @param string $category
@@ -29,9 +45,9 @@ class TricksController extends AppController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function show(RegistryInterface $doctrine, FormFactoryInterface $formBuilder, string $slug, string $category)
+    public function show(FormFactoryInterface $formBuilder, string $slug, string $category)
     {
-        $trick = $doctrine
+        $trick = $this->doctrine
             ->getRepository(Trick::class)
             ->findOneBy(["slug" => $slug]);
 
@@ -40,7 +56,7 @@ class TricksController extends AppController
                 ->add('comment', TextareaType::class, ['label' => 'Laisser un commentaire'])
                 ->getForm();
 
-            $comments = $doctrine
+            $comments = $this->doctrine
                 ->getRepository(Comment::class)
                 ->findBy(['trick' => $trick], ['updatedAt' => 'desc']);
 
@@ -51,6 +67,54 @@ class TricksController extends AppController
             ]);
         } else {
             return new RedirectResponse('/accueil', RedirectResponse::HTTP_FOUND);
+        }
+    }
+
+    /**
+     * @Route("/ajouter", name="add")
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return Response|RedirectResponse
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function add(Request $request, SessionInterface $session)
+    {
+        if ($this->isConnected()) {
+            $essai = $this->doctrine->getRepository(Trick::class)
+                ->find(1);
+
+            $trick = new Trick();
+
+            $form = $this->createForm(TrickType::class, $trick);
+            $form->remove('createdAt')
+                ->remove('updatedAt')
+                ->remove('user');
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                /** @var User $userConnected */
+                $userConnected = $this->doctrine->getRepository(User::class)
+                    ->find($session->get('user')->getId());
+
+                $trick->setCreatedAt(new DateTime('now'))
+                    ->setUpdatedAt($trick->getCreatedAt())
+                    ->setUser($userConnected);
+
+                $manager = $this->doctrine->getManager();
+                $manager->persist($trick);
+                $manager->flush();
+
+                return new RedirectResponse('/accueil');
+            }
+
+            return $this->render('/tricks/add.html.twig', [
+                'form' => $form->createView()
+            ]);
+        } else {
+            return new RedirectResponse('/accueil');
         }
     }
 }
---
+++ src/Form/TrickType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Trick;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('description')
+            ->add('published')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('category', EntityType::class, ['class' => Category::class, 'choice_label' => 'name'])
+            ->add('user', EntityType::class, ['class' => User::class, 'choice_label' => 'pseudo'])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
